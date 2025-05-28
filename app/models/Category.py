b8765e6b5962275d358c9edd1885e35d0012d174
Ernest Kamau <1.kamauernest@gmail.com>
2025-05-28 08:05:47 +0300

    generating my db

---
+++ app/models/Category.py
@@ -0,0 +1,14 @@
+from . import Base
+from sqlalchemy import Integer, String, Column, CheckConstraint
+from sqlalchemy.orm import relationship, backref
+
+class Category(Base):
+    __tablename__ = 'categories'
+    
+    id = Column(Integer(), primary_key=True)
+    category_name = Column(String(), index=True)
+    
+    products = relationship('Product', backref=backref('category'))
+    
+    def __repr__(self):
+        return f"<Category: {self.id}, {self.name}>"
