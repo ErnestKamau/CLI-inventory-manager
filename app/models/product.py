0177f3e419c84f33a7d49fa7ce25bdb38f1e46c0
Ernest Kamau <1.kamauernest@gmail.com>
2025-05-28 10:15:38 +0300

    rafactoring models

--- app/models/product.py
+++ app/models/product.py
@@ -6,7 +6,7 @@ class Product(Base):
     __tablename__ = 'products'
    
     id = Column(Integer(), primary_key=True)
-    product_name = Column(String(), index=True, nullable=False)
+    product_name = Column(String(), index=True)
     price = Column(Float(), default=0)
     stock_quantity = Column(Integer(), default=0)
     category_id = Column(Integer(), ForeignKey('categories.id'))
--- app/models/sale_item.py
+++ app/models/sale_item.py
@@ -7,8 +7,9 @@ class SaleItem(Base):
     id = Column(Integer(), primary_key=True)
     quantity = Column(Integer(), nullable=False)
     unit_price = Column(Float(), nullable=False)
-    sale_id = Column(Integer(), ForeignKey('sales.id'))
-    product_id = Column(Integer(), ForeignKey('products.id'))
+    sale_id = Column(Integer(), ForeignKey('sales.id'), nullable=False)
+    product_id = Column(Integer(), ForeignKey('products.id'), nullable=False)
     
     def __repr__(self):
         return f"<SaleItem {self.id} : quantity={self.quantity}, price={self.unit_price}, sale_id={self.sale_id}, product_id={self.product_id}"
+    
